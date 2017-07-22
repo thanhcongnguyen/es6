278ae7ec471593d2a246ac68b7bef132e8c61dfe
thanhcongnguyen <thanhcong150593@gmail.com>
2017-07-23 00:34:23 +0700

    done

---
+++ spread.js
@@ -0,0 +1,39 @@
+#syntax
+        function: myFunction(...obj)
+        array: [...obj, 4, 5, 6]
+        object: let newObj = {...obj}
+
+        //  function
+        // const args=[1,6,7]
+        // function myFunction(a,b,c,d){
+        // 	return a+b+c-d
+        // }
+        // var result = myFunction(...args,10)
+        // console.log(result)
+
+
+        //array
+        // var newArray = ["one","two",...args,"three"]
+        // console.log(newArray)
+        // copy an array
+        // var newArgs = [...args]
+        // console.log(newArgs)
+        // var arr1 = [1,2,3]
+        // var arr2 = ["one", "two", "three"]
+        // var mergeArray = [...arr2, ...arr1]
+        // console.log(mergeArray) 
+
+
+
+        //object
+        // var obj1 = { name: 'thanhcong', ago: 24 }
+        // var obj2 = { street: 'chien thang', sdt:'01634963339'}
+        //clone object
+        // var cloneObj = { ...obj1 }
+        // console.log(cloneObj)
+        // merge object
+        // var mergeObj = { ...obj1, ...obj2 }
+        // console.log(mergeObj)
+        // error : var arrObj = [...obj1]
+
+
